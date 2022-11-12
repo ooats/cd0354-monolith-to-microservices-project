e06e9aae48b57a7d3ac5132a2e477471da88c0ac
ooats <ooats77@gmail.com>
2022-11-12 21:53:16 +0200

    .

--- set_env.sh
+++ set_env.sh
@@ -2,8 +2,8 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=postgres
-export POSTGRES_HOST=udagram.cv9fuge554tc.us-east-1.rds.amazonaws.com     
-export POSTGRES_DB=Udagram
+export POSTGRES_HOST=udagram.ckzqqehtqitb.us-east-1.rds.amazonaws.com   
+export POSTGRES_DB=udagram
 export AWS_BUCKET=arn:aws:s3:::apidev267-dev2022
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
